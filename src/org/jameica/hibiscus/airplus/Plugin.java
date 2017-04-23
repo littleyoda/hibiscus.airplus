b1fc14aeb8607aeaee1e50a01418a08650859088
Sven Bursch-Osewold <sb_git@bursch.com>
2017-04-23 15:56:36 +0200

    Https Umstellung

--- src/org/jameica/hibiscus/airplus/Plugin.java
+++ src/org/jameica/hibiscus/airplus/Plugin.java
@@ -1,6 +1,13 @@
 package org.jameica.hibiscus.airplus;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import de.willuhn.jameica.messaging.TextMessage;
 import de.willuhn.jameica.plugin.AbstractPlugin;
+import de.willuhn.jameica.services.RepositoryService;
+import de.willuhn.jameica.system.Application;
+import de.willuhn.util.ApplicationException;
 
 /**
  * Basis-Klasse des Plugins.
@@ -8,6 +15,35 @@ import de.willuhn.jameica.plugin.AbstractPlugin;
 public class Plugin extends AbstractPlugin
 {
 
+
+
+
+	@Override
+	public void init() throws ApplicationException
+	{
+		super.init();
+		try {
+			URL oldRepo = new URL("http://www.open4me.de/hibiscus/");
+			URL oldRepo2 = new URL("http://www.open4me.de/hibiscus");
+			URL newRepo = new URL("https://www.open4me.de/hibiscus/");
+
+			RepositoryService s = Application.getBootLoader().getBootable(RepositoryService.class);
+
+			if (s.getRepositories().contains(oldRepo) || s.getRepositories().contains(oldRepo2)) {
+				s.removeRepository(oldRepo);
+				s.removeRepository(oldRepo2);
+				s.addRepository(newRepo);
+
+				TextMessage popup = new TextMessage();
+				popup.setTitle("Repository aktualisiert");
+				popup.setText("Das Plugin-Repository " + oldRepo + " wurde automatisch zur Erhöhung der Sicherheit auf https umgestellt!"); 
+				Application.getMessagingFactory().getMessagingQueue("jameica.popup").sendMessage(popup);
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
 
 
